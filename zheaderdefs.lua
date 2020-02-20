8bbf1202295853a691eac31cc6ebe147fcb56a44
W Livi <wnivek@gmail.com>
2020-02-20 00:15:05 -0500

    Starting on feature-reporting

--- zheaderdefs.lua
+++ zheaderdefs.lua
@@ -1,49 +1,49 @@
 local zheaderdefs = {}
 zheaderdefs.addresses = {
-	bottom				=0x00, -- start of header
-	top						=0x39, -- end of header
+	bottom			=0x00, -- start of header
+	top				=0x39, -- end of header
 	
-	version				=0x00, -- 1 byte
-	flags1				=0x01, -- 1 byte? (v3+)
-	himem					=0x04, -- byte addr; start of high memory
+	version			=0x00, -- 1 byte
+	flags1			=0x01, -- 1 byte? (v3+)
+	himem			=0x04, -- byte addr; start of high memory
 	
 	entrypoint		=0x06, -- byte addr; program counter initial value (most versions) OR
 	entryroutine	=0x06, -- packed routine address; "main" routine (v6)
 	
 	dictionary		=0x08, -- byte addr; dictionary
-	objects				=0x0a, -- byte addr; object table
+	objects			=0x0a, -- byte addr; object table
 	globalvars		=0x0c, -- byte addr; global variables table
-	static				=0x0e, -- byte addr; start of static memory (addresses below this are dynamic)
+	static			=0x0e, -- byte addr; start of static memory (addresses below this are dynamic)
 	
-	flags2				=0x10, -- 2 bytes?
-	synonyms			=0x18, -- byte addr; first byte of 'synonyms table' (v2+)
-	length				=0x1a, -- 2 bytes (divided by constant) (v3+)
-	checksum			=0x1c, -- 2 bytes (v3+)
-	intrnum				=0x1e, -- 1 byte (v4+)
-	intrver				=0x1f, -- 1 byte (v4+) 
+	flags2			=0x10, -- 2 bytes?
+	synonyms		=0x18, -- byte addr; first byte of 'synonyms table' (v2+)
+	length			=0x1a, -- 2 bytes (divided by constant) (v3+)
+	checksum		=0x1c, -- 2 bytes (v3+)
+	platform		=0x1e, -- 1 byte (v4+)
+	version			=0x1f, -- 1 byte (v4+) 
 	
-	screeny_chars =0x20, -- 1 byte; height of screen in characters (255 = infinite)
-	screenx_chars =0x21, -- 1 byte; width of screen in characters
-	screenx_units =0x22, -- 2 bytes; width of screen in 'units'
-	screeny_units =0x24, -- 2 bytes; height of screen in 'units'
-	fontx					=0x26, -- 1 byte; width of a zero in 'units' (swapped with fonty in v6)
-	fonty					=0x27, -- 1 byte; height of a character in 'units' (swapped with fontx in v6)
-	roffset				=0x28, -- Routines offset, divided by eight (v6)
-	soffset				=0x2a, -- Static strings offset, divided by eight (v6)
+	screeny_chars	=0x20, -- 1 byte; height of screen in characters (255 = infinite)
+	screenx_chars	=0x21, -- 1 byte; width of screen in characters
+	screenx_units	=0x22, -- 2 bytes; width of screen in 'units'
+	screeny_units	=0x24, -- 2 bytes; height of screen in 'units'
+	fontx			=0x26, -- 1 byte; width of a zero in 'units' (swapped with fonty in v6)
+	fonty			=0x27, -- 1 byte; height of a character in 'units' (swapped with fontx in v6)
+	roffset			=0x28, -- Routines offset, divided by eight (v6)
+	soffset			=0x2a, -- Static strings offset, divided by eight (v6)
 	colorbg_def		=0x2c, -- 1 byte; default background color (v5)
 	colorfg_def		=0x2d, -- 1 byte; default forground color (v5)
 	terminators		=0x2e, -- byte addr; terminating characters table
 	
-	str3x					=0x30, -- 2 bytes; total width in px of text sent to output stream 3
-	standard			=0x32, -- 2 bytes; Standard revision number (leave as zeroes unless )
-	alpha					=0x34, -- byte addr; 'Alphabet Table' (if 0, usedefault)
-	ext						=0x36, -- byte addr; header extension table
+	str3x			=0x30, -- 2 bytes; total width in px of text sent to output stream 3
+	standard		=0x32, -- 2 bytes; Standard revision number (leave as zeroes unless )
+	alpha			=0x34, -- byte addr; 'Alphabet Table' (if 0, usedefault)
+	ext				=0x36, -- byte addr; header extension table
 }
 zheaderdefs.storyByteEdits = {
 	zheaderdefs.addresses.flags2 = 0x07;
 }
 
-zheaderdefs.intrnums = { 1=
+zheaderdefs.platformNames = { 1=
 	'DECSystem-20',
 	'Apple IIe',
 	'Macintosh',
@@ -56,6 +56,11 @@ zheaderdefs.intrnums = { 1=
 	'Apple IIgs',
 	'Tandy Color',
 }
+zheaderdefs.platformNumbers = {}
+for key,value in pairs(t) do
+	zheaderdefs.platformNumbers[value] = key
+end
+
 zheaderdefs.extAddresses = {
 	size=0,
 	mousex=1,
--- zmachine.lua
+++ zmachine.lua
@@ -4,4 +4,40 @@ local zmachine = {}
 zmachine.memory = require("memory")
 zmachine.opdecoder = require("opdecoder")
 
+zmachine.platform = 'Atari ST'	-- number 6
+zmachine.version = 0x30	-- ASCII 0
+
+zmachine.feature_capability = {
+	-- flags 1, ver 1-3
+	nostatusbar		= false,
+	screensplit		= false,
+	fontvardef		= false,
+	
+	-- flags 1, ver 4+
+	colors			= false,
+	pics			= false,
+	fontbold		= false,
+	fontitalic		= false,
+	fontfix			= true,
+	sound			= false,
+	timedkbd		= false,
+}
+
+zmachine.feature_requestability = {
+	transcripting	= true,
+	fontfixpls		= true,
+	--redrawpls		= nil,	-- this one the *interpreter* sets to ask something of the *story*
+	picspls			= false,
+	undopls			= true,
+	mousepls		= false,
+	--colorpls		= false,	-- interpreter doesn't explicitly reject this value?  weird, but okay
+	soundpls		= false,
+	menupls			= false,
+}
+
+function zmachine:initHeader()
+	-- the purpose of this method is to let the storyfile know what capabilities this interpreter has.
+	
+end
+
 return zmachine
